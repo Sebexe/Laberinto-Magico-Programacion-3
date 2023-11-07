e99961d6f127cbb3f657a8d2c61b7a1747e264ba
sebas <sebyex18@gmail.com>
2023-11-07 18:53:26 -0300

    raro

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from funciones import *
 
 
-def solucion(f: int, c: int, laberinto: list[list[str]], matrizVis: list[list[int]], contador: int, mejor_solucion: SolucionOptima, salida: tuple, portales: dict):
+def solucion(f: int, c: int, laberinto: list[list[str]], matrizVis: list[list[int]], contador: int, mejor_solucion: SolucionOptima, salida: tuple):
     distancia_acumulada = calcular_distancia((f, c), salida)
-    posiblePortal = distanciaPortal((f, c), buscarPortales(laberinto), salida)
+    portales = buscarPortales(laberinto)
+    posiblePortal = distanciaPortal((f, c), portales, salida)
     if (contador - posiblePortal >= mejor_solucion.mejor_camino or distancia_acumulada > mejor_solucion.mejor_camino) and mejor_solucion.mejor_camino != -1:
         return
     if laberinto[f][c] == "S":
@@ -13,21 +14,26 @@ def solucion(f: int, c: int, laberinto: list[list[str]], matrizVis: list[list[in
 
     matrizVis[f][c] = 1
 
-    for i in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        if laberinto[f][c] not in ["#", ".", "E", "S"]:
-            otroPortal = buscarPortal(laberinto, (f, c), laberinto[f][c])
+    if laberinto[f][c] not in ["#", ".", "E", "S"]:
+        otroPortal = buscarPortal(laberinto, (f, c), laberinto[f][c])
+        if len(otroPortal) == 2 and matrizVis[otroPortal[0]][otroPortal[1]] != 1:
             temporalcar = laberinto[f][c]
-            if matrizVis[otroPortal[0]][otroPortal[1]] != 1:
-                laberinto[f][c] = "."
-                laberinto[otroPortal[0]][otroPortal[1]] = "."
-                solucion(otroPortal[0], otroPortal[1], laberinto,
-                         matrizVis, contador + 1, mejor_solucion, salida, portales)
+            laberinto[f][c] = "."
+            laberinto[otroPortal[0]][otroPortal[1]] = "."
+            solucion(otroPortal[0], otroPortal[1], laberinto,
+                     matrizVis, contador + 1, mejor_solucion, salida)
             laberinto[f][c] = temporalcar
             laberinto[otroPortal[0]][otroPortal[1]] = temporalcar
-        elif esPosible(f + i[0], c + i[1], laberinto) and matrizVis[f + i[0]][c + i[1]] != 1:
+
+    for i in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        if esPosible(f + i[0], c + i[1], laberinto) and matrizVis[f + i[0]][c + i[1]] != 1:
             solucion(f + i[0], c + i[1], laberinto, matrizVis,
-                     contador + 1, mejor_solucion, salida, portales)
+                     contador + 1, mejor_solucion, salida)
     matrizVis[f][c] = 0
+
+    # Avoid infinite recursion when there are no more valid moves
+    if contador == 0:
+        return
 
 
 def main():
@@ -38,9 +44,10 @@ def main():
         [0 for _ in range(len(laberinto[0]))] for _ in range(len(laberinto))]
     mejor_solucion = SolucionOptima()
     solucion(entrada[0], entrada[1], laberinto,
-             matrizVisitados, 0, mejor_solucion, salida, buscarPortales(laberinto))
+             matrizVisitados, 0, mejor_solucion, salida)
     print("Mejor solución:", mejor_solucion.mejor_camino)
 
 
 if __name__ == "__main__":
     main()
+
